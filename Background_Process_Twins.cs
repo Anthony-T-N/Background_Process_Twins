2ecf929ee4f8e8c79f86d21ebb4617cb4b961d9a
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2020-12-16 14:49:51 +1030

    Update Background_Process_Twins.cs

--- Background_Process_Twins.cs
+++ Background_Process_Twins.cs
@@ -4,24 +4,40 @@ using System.Diagnostics;
 /*
 Planning / Design
 - Read through running processes on Windows
-- Check whether twin proccess is running (If not, execute it)
+- Check whether twin process is running (If not, execute it)
 */
 
 namespace Background_Process_Twins
 {
-    class Background_Process_Twins
+    class Background_Process_Twin
     {
         static void Main(string[] args)
         {
+            Background_Process_Twin main_program = new Background_Process_Twin();
+            main_program.check_processes();
+        }
+        // Currently polling. (Bad).
+        public void check_processes()
+        {
             Process[] allProcesses = Process.GetProcesses();
-            for (int i = 0; i <= allProcesses.Length - 1; i++)
+            bool process_alive = false;
+            while (process_alive == false)
             {
-                Console.WriteLine(allProcesses[i]);
-                if (allProcesses[i].ToString().Contains("Background_Process_Twins"))
+                for (int i = 0; i <= allProcesses.Length - 1; i++)
                 {
-                    Console.WriteLine("[*] Twin Process Alive");
+                    Console.WriteLine(allProcesses[i]);
+                    if (allProcesses[i].ToString().Contains("Background_Process_Twin-2"))
+                    {
+                        Console.WriteLine("[*] Twin Process Is Alive");
+                        process_alive = true;
+                        break;
+                    }
                 }
             }
+        }
+        private void kill_switch()
+        {
+
         }
     }
 }
