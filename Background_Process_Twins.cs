0ab18cf735a1c255280a5015e15fb0d40d5e9119
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2020-12-17 15:56:03 +1030

    Update Background_Process_Twins.cs

--- Background_Process_Twins.cs
+++ Background_Process_Twins.cs
@@ -1,12 +1,10 @@
 using System;
 using System.Diagnostics;
-
 /*
 Planning / Design
 - Read through running processes on Windows
 - Check whether twin process is running (If not, execute it)
 */
-
 namespace Background_Process_Twins
 {
     class Background_Process_Twin
@@ -14,26 +12,34 @@ namespace Background_Process_Twins
         static void Main(string[] args)
         {
             Background_Process_Twin main_program = new Background_Process_Twin();
-            main_program.check_processes();
+            if (main_program.check_processes() == false)
+            {
+                Console.WriteLine("[*] Twin Process Not Alive");
+                main_program.execute_twin_process();
+            }
+            Console.WriteLine("[*] END");
         }
         // Currently polling. (Bad).
-        public void check_processes()
+        public bool check_processes()
         {
             Process[] allProcesses = Process.GetProcesses();
             bool process_alive = false;
-            while (process_alive == false)
+            for (int i = 0; i <= allProcesses.Length - 1; i++)
             {
-                for (int i = 0; i <= allProcesses.Length - 1; i++)
+                Console.WriteLine(allProcesses[i]);
+                if (allProcesses[i].ToString().Contains("Background_Process_Twin_2"))
                 {
-                    Console.WriteLine(allProcesses[i]);
-                    if (allProcesses[i].ToString().Contains("Background_Process_Twin-2"))
-                    {
-                        Console.WriteLine("[*] Twin Process Is Alive");
-                        process_alive = true;
-                        break;
-                    }
+                    Console.WriteLine("[*] Twin Process Is Alive [2]");
+                    process_alive = true;
+                    break;
                 }
             }
+            return process_alive;
+        }
+        public void execute_twin_process()
+        {
+            Process.Start(@"---");
+            Console.WriteLine("[+] Twin Process Executed");
         }
         private void kill_switch()
         {
